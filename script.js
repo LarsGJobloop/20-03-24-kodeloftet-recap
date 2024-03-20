d3cc055b465165311746562501a02d6ebe8328d4
LarsGJobloop <lars.gunnar@jobloop.no>
2024-03-20 12:11:42 +0100

    add w3school carusel example with comments

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-console.log('Hello!');
-console.log("Testing");
-const title = "Testing";
+const button = document.getElementById("test-button")
+button.addEventListener("click", handleClick)
+
+function handleClick() {
+    // Hente ut nåværende verdi
+    const display = document.querySelector("#counter-display")
+    const currentValueString = display.textContent
+    const currentValue = Number(currentValueString)
+
+    // Beregn den nye verdien
+    const newValue = currentValue + 1
+
+    // Updater Documentet
+    display.textContent = newValue
+}
